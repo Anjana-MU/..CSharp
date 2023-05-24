d61fdc5f44d0e77edef2e69facff359831a96995
Anjana-MU <77484700+Anjana-MU@users.noreply.github.com>
2023-05-24 13:12:08 +0530

    Update Ex2_Student.cs

--- Ex2_Student.cs
+++ Ex2_Student.cs
@@ -301,6 +301,61 @@ namespace Student
             StudentGroup de1 = sp2 + sp1;                                   // Total number of students : 0
             de1.CompleteInfo();
      */
+     
+     /*output2
+     *  StudentGroup dr1 = sp2 + kids_1;                     //sp2 contains s1,s2,s3,s4
+        dr1.CompleteInfo();                                 //kids_1 contains s5, s1
+        foreach (var student in dr1.StudentDetails)         //operator overloading a studentgroup + student gives a new group with unique students from both groups
+        {
+            student.StudentInfo();
+        }
+     * 
+         Group Name: Drama
+         Total number of students : 5
+
+         Student info :
+         Name: Student1 | Sex : Male | Standard : 11 | Grade : A
+
+         Student info :
+         Name: Student2 | Sex : Female | Standard : 10 | Grade : B
+
+         Student info :
+         Name: Student3 | Sex : Male | Standard : 10 | Grade : F
+
+         Student info :
+         Name: Student4 | Sex : Female | Standard : 9 | Grade : B
+
+         Student info :
+         Name: Student5 | Sex : Male | Standard : 12 | Grade : C
+     */
+
+    /*output3
+            StudentGroup de1 = sp2 + sp1;                   //sp2 contains s1,s2,s3,s4
+            de1.CompleteInfo();                             //sp1 contains s1 and s4
+            foreach (var student in de1.StudentDetails)     //operator overloading a studentgroup + studentgroup gives a new group with unique students from both groups
+            {
+                student.StudentInfo();
+            }
+
+             Group Name: Debate
+             Total number of students : 4
+
+             Student info :
+             Name: Student1 | Sex : Male | Standard : 11 | Grade : A
+
+             Student info :
+             Name: Student2 | Sex : Female | Standard : 10 | Grade : B
+
+             Student info :
+             Name: Student3 | Sex : Male | Standard : 10 | Grade : F
+
+             Student info :
+             Name: Student4 | Sex : Female | Standard : 9 | Grade : B
+     */
+
+
+}
+
 
 
 }
